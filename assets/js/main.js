49928f8e8aaa134c10c9dcdc2d3df76293713cc9
krsna <73932108+kresna-rev@users.noreply.github.com>
2022-06-18 17:19:58 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,7 +103,6 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-    dynamicBullets: true,
   },
   navigation: {
     nextEl: '.swiper-button-next',
